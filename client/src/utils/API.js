67480d42bb97dcd08f9da36743f608d0a8bcf75c
CBshmear <connorbeer13@gmail.com>
2023-04-06 18:35:48 -0500

    switched api key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,5 @@
 export const apikey = "eEWaC4GIndUneCiGtEBgtvwRfDJrPHd2RVmndhQd";
 export const searchNationalParks = () => {
-  return fetch(`https://developer.nps.gov/api/v1/campgrounds?thumbnail&api_key=vhVIbYaBAmEBUcRNAtvwWUOoxLB7SQbp5z8Pe1R6&limit=20" -H "accept: application/json
+  return fetch(`https://developer.nps.gov/api/v1/campgrounds?thumbnail&api_key=${apikey}&limit=20" -H "accept: application/json
     `);
 };
